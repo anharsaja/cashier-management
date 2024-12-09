4dfffb4dbc08c4e5e31cda2d78764d61b50f93b7
Heri Setyawan <herisetyawan233@gmail.com>
2024-12-10 04:02:47 +0700

    initial auth

---
+++ app/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+function Index() {
+  return (
+    <View style={{
+      flex: 1,
+      backgroundColor: "#fff",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center"
+    }}>
+      <Text>
+        Sign In
+      </Text>
+    </View>
+  )
+}
+
+export default Index
