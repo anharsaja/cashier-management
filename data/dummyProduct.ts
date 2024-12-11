30f3b95015658b6a914c1ab80daab2e12d1fcfba
anharsaja <anharmuk12@gmail.com>
2024-12-11 14:33:59 +0700

    feat: add cart logic

--- data/dummyProduct.ts
+++ data/dummyProduct.ts
@@ -2,11 +2,12 @@ export type Product = {
   id: number;
   name: string;
   price: number;
+  count: number;
 };
 
 export const products: Product[] = [
-  { id: 1, name: 'Alon alon asal kelepon', price: 8000 },
-  { id: 2, name: 'Kubisu', price: 8000 },
-  { id: 3, name: 'Kopi Songar', price: 7000 },
-  { id: 4, name: 'Mie Goreng Fullset', price: 10000 },
+  { id: 1, name: 'Alon alon asal kelepon', price: 8000 , count: 0},
+  { id: 2, name: 'Kubisu', price: 8000, count: 0 },
+  { id: 3, name: 'Kopi Songar', price: 7000, count: 0 },
+  { id: 4, name: 'Mie Goreng Fullset', price: 10000, count: 0 },
 ];
